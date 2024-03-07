021538033d57c3a5f73454158cb3dba35b9e9e59
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-03-07 13:40:47 +0100

    opgave 1.2

--- opgaveuge10/Program.cs
+++ opgaveuge10/Program.cs
@@ -5,15 +5,32 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            //int Sami = 27;
-            //int Peter = 26;
-            //int Robin = 24;
-            //int Sander = 24;
-            //int René = 34;
-            //int Ronald = 44;
-            
+            int Sami = 27;
+            int Peter = 26;
+            int Robin = 24;
+            int Sander = 24;
+            int René = 34;
+            int Ronald = 44;
+            int[] ages = {Sami, Peter, Robin, Sander, René, Ronald};
+
+            double gennemsnit;
+            int ageSum = 0;
+
+            foreach ( int age in ages ) 
+            {
+            Console.WriteLine(age);
+            }
+
+            foreach (int age in ages)
+            {
+                ageSum += age;
+                
+            }
+            Console.WriteLine("Summen er: " + ageSum);
 
             
+            Console.WriteLine("Gennemsnittet er: " + (double) ageSum / ages.Length);
+
 
             Console.ReadLine();
 
