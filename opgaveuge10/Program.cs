1e9830a423ebace654182ed30b2e4f87119b171a
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-03-07 13:11:46 +0100

    opgave 1.1

---
+++ opgaveuge10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace opgaveuge10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //int Sami = 27;
+            //int Peter = 26;
+            //int Robin = 24;
+            //int Sander = 24;
+            //int René = 34;
+            //int Ronald = 44;
+
+            //Console.WriteLine(Sami);
+            //Console.WriteLine(Peter);
+            //Console.WriteLine(Robin);
+            //Console.WriteLine(Sander);
+            //Console.WriteLine(René);
+            //Console.WriteLine(Ronald);
+
+            
+
+            //double gennemsnit = (double)(Sami + Peter + Robin + Sander + René + Ronald ) / 6;
+
+            //Console.WriteLine("Gennemsnitter er: " + gennemsnit);
+
+
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
