0f9584daacc00af63a54c89eb924bd5a8371b5a7
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-03-08 10:28:22 +0100

    opgave 3.3

---
+++ opgaveuge10/Menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgaveuge10
+{
+    internal class Menu
+    {
+        public string Title;
+        MenuItem[] menuItems;
+        int itemCount;
+
+        // Menu constructor
+        public Menu(string title)
+        {
+            Title = title;
+            MenuItems =  new MenuItem[10];
+            ItemCount = 0;
+        }
+
+        public class MenuItem
+        {
+            public string Title;
+
+            public MenuItem(string title)
+            {
+                Title = title;
+            }
+        }
+
+        public void AddMenuItem(string menuTitle)
+        {
+            menuItems[itemCount] = new MenuItem(menuTitle);
+            itemCount++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Title+"\n\n");
+
+            for (int item = 0; item < itemCount; item++)
+                Console.WriteLine($"{item+1}. {menuItems[item].Title}");
+
+            Console.WriteLine("\n\n(Tryk menupunkt eller 0 for at afslutte)");
+        }
+
+    }
+}
---
+++ opgaveuge10/MenuSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace opgaveuge10
+{
+    internal class MenuSystem
+    {
+        static void Main(string[] args)
+        {
+
+            //Opgave 3.3
+
+            Menu menu = new Menu(title: "Min Fantastiske Menu");
+            
+            string[] menuTitles = { "Gør dit", "Gør dat", "Gør noget", "Få svaret på livet" };
+
+            foreach (string menuTitle in menuTitles)
+                menu.AddMenuItem(menuTitle);
+
+            menu.Show();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
