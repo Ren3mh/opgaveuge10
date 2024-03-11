0fe99a5960c59bbe7b55e9686f1fba9900d48292
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-03-11 12:59:37 +0100

    opgave 3.5 rettet

---
+++ opgaveuge10/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgaveuge10
+{
+    
+
+    internal class Menu
+    {
+        public string Title;
+        MenuItem[] menuItems = new MenuItem[100]; // maksimum 100 menumpunkter
+        int itemCount;
+
+        // Menu constructor
+        public Menu(string title)
+        {
+            Title = title;
+            itemCount = 0;
+        }
+
+        public class MenuItem
+        {
+            public string Title;
+            public string PrintThis;
+
+            public MenuItem(string title, string printThis)
+            {
+                Title = title;
+                PrintThis = printThis;
+            }
+        }
+
+        public void AddMenuItem(string menuTitle, string menuPrint)
+        {
+            menuItems[itemCount] = new MenuItem(menuTitle, menuPrint);
+            itemCount++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Title+"\n\n");
+
+            for (int item = 0; item < itemCount; item++)
+                Console.WriteLine($"{item+1}. {menuItems[item].Title}");
+
+            Console.Write("\n\n(Tryk menupunkt eller 0 for at afslutte) ");
+
+        }
+
+        public void SelectMenuItem()
+        {
+            int selectedMenuItem;
+
+            while (true) 
+            {
+
+                if (int.TryParse(Console.ReadLine(), out selectedMenuItem) && selectedMenuItem <= itemCount && selectedMenuItem >= 0)
+                    break;
+
+                Console.Write("Dit valg af menupunkt eksisterer ikke. Prøv igen: ");
+            }
+
+            string message = menuItems[selectedMenuItem -1].PrintThis;
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ opgaveuge10/MenuSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace opgaveuge10
+{
+    internal class MenuSystem
+    {
+        static void Main(string[] args)
+        {
+
+            //Opgave 3.5
+
+            Menu menu = new Menu(title: "Min Fantastiske Menu");
+            
+            string[,] menuItems = { { "Gør dit", "Gør dit" }, { "Gør dat", "Gør dat" }, { "Gør noget", "Gør noget" }, { "Få svaret på livet", "42" } };
+            
+
+            for (int i = 0; i < menuItems.GetLength(0); i++)
+                menu.AddMenuItem(menuItems[i, 0], menuItems[i, 1]);
+
+            menu.Show();
+
+            menu.SelectMenuItem();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
